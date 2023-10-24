d0de19c1de6a94ebfd3a627c1dbcaa6d9a2dc9d3
Aurora Sanderson <AuroraTuring@gmail.com>
2023-10-24 14:04:16 -0600

    Delete file

--- arrays.rb
+++
@@ -1,9 +0,0 @@
-friends = ["Akane", "Miles", "Jessie", "Shandra"]
-ages = [30, 31, 32, 33]
-temperatures = [98.6, 99.5, 100.4, 101.3]
-sick = [true, false, true, false]
-
-
-# [Intentionally open-ended] Demonstrate your understanding of index positions in this file. You can write an explanation, provide some examples with the Arrays you’ve created, or anything else.
-
-# 
