167e3898902e4284b26f135aad459ab44e66e965
Aurora Sanderson <AuroraTuring@gmail.com>
2023-10-24 13:52:16 -0600

    Complete assignment

---
+++ practice/arrays.rb
@@ -0,0 +1,19 @@
+friends = ["Akane", "Miles", "Jessie", "Shandra"]
+ages = [30, 31, 32, 33]
+temperatures = [98.6, 99.5, 100.4, 101.3]
+sick = [true, false, true, false]
+
+
+# [Intentionally open-ended] Demonstrate your understanding of index positions in this file. You can write an explanation, provide some examples with the Arrays you’ve created, or anything else.
+
+# friends[0] would call Akane.
+puts friends[0] 
+
+# ages[-1] would call 33. 
+puts ages[-1] 
+
+# temperatures[3] would call 101.3.
+puts temperatures[3]
+
+# sick[2] would call true. 
+puts sick[2]
